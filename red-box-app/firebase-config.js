baf3f870b5e5d387207700e8d14953c5be59f3bd
therealkoil <kylemaroukis9@gmail.com>
2026-03-18 18:18:14 -0400

    Delete red-box-app directory

--- red-box-app/firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.RED_BOX_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBY_P-fBZik1aYi9qVHYbNnCzkKxDGanX0",
-  authDomain: "flickzone-dad82.firebaseapp.com",
-  projectId: "flickzone-dad82",
-  storageBucket: "flickzone-dad82.firebasestorage.app",
-  messagingSenderId: "699699760758",
-  appId: "1:699699760758:web:2cb52f9bba388e7514d957",
-  measurementId: "G-XBNQF0NGP5"
-};
